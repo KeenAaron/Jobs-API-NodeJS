034ebf3a2e4fb2d5b0132926c2267cd02eb479ea
Keen Aaron <keenabril@gmail.com>
2023-09-09 18:35:29 +0200

    Jobs API with NodeJS, Express, mongoose and MongoDB

---
+++ controllers/auth.js
@@ -0,0 +1,39 @@
+const { StatusCodes } = require('http-status-codes')
+const User = require('../models/User')
+const { BadRequestError, UnauthenticatedError } = require('../errors') 
+
+const register = async (req, res) => {
+    const user = await User.create({ ...req.body })
+    const token = user.createJWT()
+
+    res.status(StatusCodes.CREATED).json({ user: { name: user.name }, token })
+}
+
+const login = async (req, res) => {
+    const { email, password } = req.body
+    
+    if (!email || !password) {
+        throw new BadRequestError('Plese provide email and password')
+    }
+
+    const user = await User.findOne({ email })
+
+    if (!user) {
+        throw new UnauthenticatedError('Invalid email')
+    }
+
+    const isPassword = await user.checkPassword(password);
+
+    if (!isPassword) {
+        throw new UnauthenticatedError('Invalid password')
+    }
+
+    const token = user.createJWT();
+    
+    res.status(StatusCodes.OK).json({ user: { name: user.name }, token })
+}
+
+module.exports = {
+    register,
+    login,
+}
